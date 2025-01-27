3cbdbd1ac198781afc6a37fcff4081505a93c5ea
lilrabbitcodes <168262429+lilrabbitcodes@users.noreply.github.com>
2025-01-27 18:04:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 import streamlit as st
-
-# Basic page config must be first
-st.set_page_config(page_title="Test App")
-
-# Debug message comes after
-st.write("Testing basic app...")
-
-# Simple test
-st.title("Hello World")
-if st.button("Click me"):
-    st.write("Button clicked!")
-
 st.write("Hello World")
