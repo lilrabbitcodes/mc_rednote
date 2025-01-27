77864736ad9851d9932d08d13cef47a4a8092e29
lilrabbitcodes <168262429+lilrabbitcodes@users.noreply.github.com>
2025-01-27 20:23:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ st.markdown("""
 
 /* Audio player styling */
 .stAudio {
-    width: 50% !important;
+    width: 100% !important;
     margin: 5px auto !important;
     display: flex !important;
     justify-content: center !important;
